1ee733124e773007b71ed6741b3e7b70d490ce7f
Dharaneshwari <dharaneshwarijjj@gmail.com>
2026-04-07 00:22:16 +0530

    Updated baseURL to Render production link

--- pawpass-frontend/src/api/client.js
+++ pawpass-frontend/src/api/client.js
@@ -3,7 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const apiClient = axios.create({
   // FIXED: Removed the space after http:// to ensure valid URL formatting
-  baseURL: 'http://192.168.137.1:5000/api', 
+  baseURL: 'https://pawpass-app-2.onrender.com/api',
+  
   headers: { 'Content-Type': 'application/json' },
 });
 
